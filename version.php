79de153e9e816d45df631e91b92526cf9e48c1d7
Manuel <manuel@wiris.com>
2016-08-02 10:39:01 +0200

    3.59.1.1310 Stable version

--- version.php
+++ version.php
@@ -19,8 +19,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016080100;
-$plugin->release = '3.59.0.1309';
+$plugin->version = 2016080200;
+$plugin->release = '3.59.1.1310';
 $plugin->requires =  2011120511;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'filter_wiris';
