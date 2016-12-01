ddbf9df2b54bfe32c7b586023bff7f75a009a9a5
dagobertorobayorodriguez <dagoberto.robayo@nivel7.net>
2016-12-01 10:06:52 -0500

    Relative call and indentation
    Correct relative call and adjust indentation, remove spaces to spare

--- integration/showimage.php
+++ integration/showimage.php
@@ -1,8 +1,7 @@
 <?php
-
 // ${license.statement}
-
-require_once 'pluginbuilder.php';
+require_once '../../../config.php';
+require_once $CFG->dirroot . '/filter/wiris/integration/pluginbuilder.php';
 
 $provider = $pluginBuilder->getCustomParamsProvider();
 
@@ -16,8 +15,8 @@ $lang = $provider->getParameter('lang', 'en');
 // showimage.php?formula.png --> showimage.php?formula
 // because formula is md5 string, remove all extensions.
 if (!is_null($digest)) {
-	$a = explode(".", $digest);
-	$digest = array_shift($a);
+    $a = explode(".", $digest);
+    $digest = array_shift($a);
 }
 
 // Adding - if necessary - CORS headers
@@ -25,22 +24,21 @@ $origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : "";
 $res = new com_wiris_system_service_HttpResponse();
 $pluginBuilder->addCorsHeaders($res, $origin);
 
-
 if ($pluginBuilder->getConfiguration()->getProperty("wirispluginperformance", "false") == "true") {
-   header("Content-type: application/json");
-   $secondsToCache = 3600;
-   $ts = gmdate("D, d M Y H:i:s", time() + $secondsToCache) . " GMT";
-   header("Expires: $ts");
-   header("Cache-Control: max-age=$secondsToCache");
-	// If digest == null formula is not in cache.
-	if (is_null($digest)) {
-		$render->showImage(null, $mml, $provider);
-	    $digest = $render->computeDigest($mml, $provider->getRenderParameters($pluginBuilder->getConfiguration()));
-	}
-   $r = $render->showImageJson($digest, $lang);
+    header("Content-type: application/json");
+    $secondsToCache = 3600;
+    $ts = gmdate("D, d M Y H:i:s", time() + $secondsToCache) . " GMT";
+    header("Expires: $ts");
+    header("Cache-Control: max-age=$secondsToCache");
+    // If digest == null formula is not in cache.
+    if (is_null($digest)) {
+	$render->showImage(null, $mml, $provider);
+	$digest = $render->computeDigest($mml, $provider->getRenderParameters($pluginBuilder->getConfiguration()));
+    }
+    $r = $render->showImageJson($digest, $lang);
 } else {
-	$contentType = $pluginBuilder->getImageFormatController()->getContentType();
-	header('Content-Type: ' . $contentType);
-	$r = $render->showImage($digest, $mml, $provider);
+    $contentType = $pluginBuilder->getImageFormatController()->getContentType();
+    header('Content-Type: ' . $contentType);
+    $r = $render->showImage($digest, $mml, $provider);
 }
 echo $r;
