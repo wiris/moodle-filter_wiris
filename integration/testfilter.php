02811010ac62d95946b78b355479d615c6cc7cbb
dagobertorobayorodriguez <dagoberto.robayo@nivel7.net>
2016-12-01 10:09:03 -0500

    Relative call and remove spaces
    Correct relative require_once and remove a spaces to spare

--- integration/testfilter.php
+++ integration/testfilter.php
@@ -1,11 +1,10 @@
 <?php
 // ${license.statement}
 
-
 // Please, set if called from the command line
 // $_SERVER['SCRIPT_NAME'] = "/generic/integration/filter.php";
-
-require_once 'pluginbuilder.php';
+require_once '../../../config.php';
+require_once $CFG->dirroot . '/filter/wiris/integration/pluginbuilder.php';
 $text = $pluginBuilder->newTextService();
 $input = "<html><body><b>Formula: </b><math><mfrac><mi>x</mi><mn>1000</mn></mfrac></math></body></html>";
 $params = null;
