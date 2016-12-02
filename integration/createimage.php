342b974e56611c5e6723105be5199b6a6abed665
dagobertorobayorodriguez <dagoberto.robayo@nivel7.net>
2016-12-02 15:15:32 -0500

    Add space, return require_once
    Add a missing space and back previous require_once, add punctuation

--- integration/createimage.php
+++ integration/createimage.php
@@ -1,7 +1,6 @@
 <?php
 // ${license.statement}
-require_once '../../../config.php';
-require_once $CFG->dirroot . '/filter/wiris/integration/pluginbuilder.php';
+require_once ('pluginbuilder.php');
 
 $provider = $pluginBuilder->getCustomParamsProvider();
 
@@ -14,8 +13,8 @@ if (get_magic_quotes_gpc() == 1) {
     $PARAMS = array_map('stripslashes', $PARAMS);
 }
 
-// Adding - if necessary - CORS headers
-$origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : "";
+// Adding - if necessary - CORS headers.
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : "";
 $res = new com_wiris_system_service_HttpResponse();
 $pluginBuilder->addCorsHeaders($res, $origin);
 
