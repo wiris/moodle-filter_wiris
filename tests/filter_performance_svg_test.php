e935771827b5e705a6655fa1b0b6dc2a68b4f9f1
Manuel <manuel@wiris.com>
2017-08-02 09:21:52 +0200

    2017080200 development version

--- tests/filter_performance_svg_test.php
+++ tests/filter_performance_svg_test.php
@@ -27,62 +27,91 @@ defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
 require_once($CFG->dirroot . '/filter/wiris/filter.php');
+require_once($CFG->dirroot . '/filter/wiris/integration/lib/com/wiris/system/CallWrapper.class.php');
 
-class filter_wiris_filter_noperformance_png_testcase extends advanced_testcase
+class filter_wiris_filter_performance_svg_testcase extends advanced_testcase
 {   protected $wirisfilter;
     protected $safexml;
     protected $xml;
     protected $image;
-    protected $instance;
     protected $cachetable;
 
     protected function setUp() {
-        global $CFG;
         parent::setUp();
         $this->resetAfterTest(true);
-        filter_wiris_pluginwrapper::set_configuration(array('wirispluginperformance' => 'false',
-                                                            'wirisimageformat' => 'png'));
+        filter_wiris_pluginwrapper::set_configuration(array('wirispluginperformance' => 'true',
+                                                            'wirisimageformat' => 'svg'));
         $this->wirisfilter = new filter_wiris(context_system::instance(), array());
         $this->cachetable = 'filter_wiris_formulas';
         $this->safexml = '«math xmlns=¨http://www.w3.org/1998/Math/MathML¨»«mn»1«/mn»«mo»+«/mo»«mn»2«/mn»«/math»';
         $this->xml = '<math xmlns="http://www.w3.org/1998/Math/MathML"><mn>1</mn><mo>+</mo><mn>2</mn></math>';
 
+        // Image: print("asd");//
+        $this->specialcharsimagesafexml = '«math xmlns=¨http://www.w3.org/1998/Math/MathML¨»«mi»p«/mi»«mi»r«/mi»«mi»i«/mi»';
+        $this->specialcharsimagesafexml .= '«mi»n«/mi»«mi»t«/mi»«mo»(«/mo»«mo»§#34;«/mo»«mi»a«/mi»«mi»s«/mi»«mi»d«/mi»«mo»§#34;';
+        $this->specialcharsimagesafexml .= '«/mo»«mo»)«/mo»«mo»;«/mo»«mo»/«/mo»«mo»/«/mo»«/math»';
+
+        // Special image svg
+        $this->imagesvgspecialchars = 'data:image/svg+xml;charset=utf8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2F';
+        $this->imagesvgspecialchars .= 'svg%22%20xmlns%3Awrs%3D%22http%3A%2F%2Fwww.wiris.com%2Fxml%2Fcvs-extension%22%20height';
+        $this->imagesvgspecialchars .= '%3D%2221%22%20width%3D%22117%22%20wrs%3Abaseline%3D%2216%22%3E%3C%21--MathML%3A%20%3Cmat';
+
+        // Special chars alt
+        $this->specialcharsalt = '{"result":{"text":"p r i n t left parenthesis \" a s d \" right parenthesis semicolon divided by divided by"},"status":"ok"}';
+
         // Simple images of "1+2".
 
-        // Png format.
-        $testsiteprotocol = strrpos($CFG->wwwroot, 'https') !== false ? 'https' : 'http';
-        $this->imagepng = '<img src="' . $testsiteprotocol . '://www.example.com/moodle/filter/wiris/integration/showimage.php';
-        $this->imagepng .= '?formula=cd345a63d1346d7a11b5e73bb97e5bb7&refererquery=?course=1/category=0"';
-        $this->imagepng .= ' class="Wirisformula" alt="1 plus 2" width="37" height="13" style="vertical-align:-1px"';
-        $this->imagepng .= ' data-mathml=\'«math ';
-        $this->imagepng .= 'xmlns=¨http://www.w3.org/1998/Math/MathML¨»«mn»1«/mn»«mo»+«/mo»«mn»2«/mn»«/math»\'/>';
+        // Svg performance.
+        $this->imagesvgperformance = 'data:image/svg+xml;charset=utf8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3';
+        $this->imagesvgperformance .= 'Awrs%3D%22http%3A%2F%2Fwww.wiris.com%2Fxml%2Fcvs-extension%22%20height%3D%2220';
+        $this->imagesvgperformance .= '%22%20width%3D%2234%22%20wrs%3Abaseline%3D%2216%22%3E%3C%21--MathML%3A%20%3Cmath%20xmlns%3D%22http%3A%2F%2Fww';
+        $this->imagesvgperformance .= 'w.w3.org%2F1998%2FMath%2FMathML%22%3E%3Cmn%3E1%3C%2Fmn%3E%3Cmo%3E%2B%3C%2';
 
-        // Svg format.
-        $this->imagesvg = '<img src="' . $testsiteprotocol . ' ://www.example.com/moodle/filter/wiris/integration/showimage.php';
-        $this->imagesvg .= '?formula=cd345a63d1346d7a11b5e73bb97e5bb7&refererquery=?course=1/category=0"';
-        $this->imagesvg .= ' class="Wirisformula" alt="1 plus 2" width="34" height="20" style="vertical-align:-4px"';
-        $this->imagesvg .= ' data-mathml=\'«math ';
-        $this->imagesvg .= 'xmlns=¨http://www.w3.org/1998/Math/MathML¨»«mn»1«/mn»«mo»+«/mo»«mn»2«/mn»«/math»\'/>';
+        $this->svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:wrs="http://www.wiris.com/xml/cvs-extension" height="20"';
+        $this->svg .= ' width="34" wrs:baseline="16"><!--MathML: <math xmlns="http://www.w3.org/1998/Math/MathML"><mn>1</mn><mo>';
+        $this->svg .= '+</mo><mn>2</mn></math>--><defs><style type="text/css">@font-face{font-family:';
 
-        $wirispluginwrapper = new filter_wiris_pluginwrapper();
-        $this->instance = $wirispluginwrapper->get_instance();
     }
 
-    public function test_filter_safexml_without_performance_png() {
+    public function test_filter_safexml_with_performance_svg() {
         $output = $this->wirisfilter->filter($this->safexml);
-        $this->assertEquals($output, $this->imagepng);
+        $assertion = strrpos($output, $this->imagesvgperformance) !== false;
+        $this->assertTrue($assertion);
     }
 
-    public function test_filter_xml_without_performance_png() {
+    public function test_filter_xml_with_performance() {
         $output = $this->wirisfilter->filter($this->xml);
-        $this->assertEquals($output, $this->imagepng);
+        $assertion = strrpos($output, $this->imagesvgperformance) !== false;
+        $this->assertTrue($assertion);
     }
 
-    public function test_filter_safexml_without_performance_png_cache() {
-        $output = $this->wirisfilter->filter($this->safexml);
-        $cachefile = new moodlefilecache('filter_wiris', 'images');
-        $md5 = 'cd345a63d1346d7a11b5e73bb97e5bb7';
-        $data = $cachefile->get($md5);
-        $this->assertEquals($output, $this->imagepng);
+    public function test_filter_safexml_with_performance_jsonconent_db() {
+
+        $this->wirisfilter = new filter_wiris(context_system::instance(), array());
+        $this->wirisfilter->filter($this->safexml);
+
+        global $DB;
+        $record = $DB->get_record($this->cachetable, array('md5' => 'cd345a63d1346d7a11b5e73bb97e5bb7'));
+        $assertion = strrpos($record->jsoncontent, $this->svg) !== false;
+
+        $this->assertTrue($assertion);
+    }
+
+    public function test_filter_safexml_with_performance_alt_db() {
+
+        $this->wirisfilter = new filter_wiris(context_system::instance(), array());
+        $this->wirisfilter->filter($this->specialcharsimagesafexml);
+
+        global $DB;
+        $record = $DB->get_record($this->cachetable, array('md5' => 'fc13b6ac6aec34845457b164dd4af76a'));
+
+        $this->assertEquals($this->specialcharsalt, $record->alt);
+    }
+
+    public function test_filter_xml_with_performance_special_chars() {
+        $output = $this->wirisfilter->filter($this->specialcharsimagesafexml);
+
+        $assertion = strrpos($output, $this->imagesvgspecialchars) !== false;
+        $this->assertTrue($assertion);
     }
 }
