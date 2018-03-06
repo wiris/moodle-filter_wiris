9ea449e587a24922e6cd26358c45d095f0e68e04
Manuel Cagigas <manuel@wiris.com>
2018-03-06 14:56:41 +0100

    7.0.0 version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018030500;
+$plugin->version = 2018030600;
 $plugin->release = '7.0.0.1386';
 $plugin->requires = 2011120511;
 $plugin->maturity = MATURITY_STABLE;
