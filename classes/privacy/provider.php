87db05d7cf172f8d028d988ab75df6c2816f137a
Manuel <manuel@wiris.com>
2018-04-18 00:29:07 +0200

    2018041800 development version

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -30,6 +30,7 @@ class provider implements
     // This plugin does not store any personal user data.
     \core_privacy\local\metadata\null_provider
 {
+    use \core_privacy\local\legacy_polyfill;
 
     /**
      * Get the language string identifier with the component's language
@@ -37,7 +38,7 @@ class provider implements
      *
      * @return  string
      */
-    public static function get_reason() : string {
+    public static function _get_reason() {
         return 'privacy:null_reason';
     }
 }
