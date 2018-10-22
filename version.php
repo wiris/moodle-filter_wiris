5578bc1ac62d0a0a352919c400c57948e0b2f547
Jenkins <jenkins@wiris.com>
2018-10-22 13:16:15 +0200

    2018102200 Stable version

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2018102200;
-
+$plugin->release = '7.7.2.1396';
 $plugin->requires = 2011120511;
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'filter_wiris';
 $plugin->dependencies = array();
