383a6c2a7372426c63528cb9a98840dbbb0d6cd5
Xavier Ripoll <xavier@wiris.com>
2020-05-12 11:38:21 +0200

    Use algorithm from JavaScript library for safe XML decoding

--- subfilters/mathjax.php
+++ subfilters/mathjax.php
@@ -16,148 +16,104 @@
 //
 
 /**
- * This filter doesn't make any calls to the wiris.net services, instead
- * converting safeXML into normal XML and allowing MathJax to render the
- * formulas.
- * 
- * This can be used for improved performance in servers with lots of
- * network connections.
- *
- * @package    filter
- * @subpackage wiris
- * @copyright  WIRIS Europe (Maths for more S.L)
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+* This filter doesn't make any calls to the wiris.net services, instead
+* converting safeXML into normal XML and allowing MathJax to render the
+* formulas.
+* 
+* This can be used for improved performance in servers with lots of
+* network connections.
+*
+* @package    filter
+* @subpackage wiris
+* @copyright  WIRIS Europe (Maths for more S.L)
+* @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
 
 
 defined('MOODLE_INTERNAL') || die();
 
 
 class filter_wiris_mathjax extends moodle_text_filter {
-
+    
     /**
-     * Set any context-specific configuration for this filter.
-     *
-     * @param context $context The current context.
-     * @param array $localconfig Any context-specific configuration for this filter.
-     */
+    * Set any context-specific configuration for this filter.
+    *
+    * @param context $context The current context.
+    * @param array $localconfig Any context-specific configuration for this filter.
+    */
     public function __construct($context, array $localconfig) {
         $this->context = $context;
         $this->localconfig = $localconfig;
     }
 
-    private const tags = [
-        "mathTag" => "math",
-        "in_appletclose" => "«/APPLET»",
-        "in_appletopen" => "«APPLET",
-        "out_quote" => "'",
-        "in_quote" => "`",
-        "out_entity" => "&",
-        "in_entity" => "§",
-        "out_close" => ">",
-        "in_close" => "»",
-        "out_open" => "<",
-        "in_open" => "«",
-        "out_double_quote" => "\"",
-        "in_double_quote" => "¨",
-        "in_mathclose" => "«/math»",
-        "in_mathopen" => "«math"
-    ];
-
     public function filter($text, array $options = array()) {
-        $tags = (object) $this->tags;
-        $n0 = null;
-        $n1 = null;
-        $m0 = null;
-        $m1 = null;
-        $output = null;
-        $sub = null;
-        $output = new StringBuf();
-        $n0 = 0;
-        $n1 = $this->_hx_index_of($text, $tags->in_mathopen, $n0);
-        $tag = "data-mathml"; //$this->plugin->getConfiguration()->getProperty(com_wiris_plugin_api_ConfigurationKeys::$EDITOR_MATHML_ATTRIBUTE, "data-mathml");
-        $dataMathml = $this->_hx_index_of($text, $tag, 0);
-        while($n1 >= 0) {
-            $m0 = $n0;
-            $output->add($this->_hx_substr($text, $n0, $n1 - $n0));
-            $n0 = $n1;
-            $n1 = $this->_hx_index_of($text, $tags->in_mathclose, $n0);
-            if($n1 >= 0) {
-                $n1 = $n1 + strlen($tags->in_mathclose);
-                $sub = $this->_hx_substr($text, $n0, $n1 - $n0);
-                if($dataMathml !== -1) {
-                    $m1 = $this->_hx_index_of($text, "/>", $n1);
-                    if($m1 === -1) {
-                        $m1 = $this->_hx_index_of($text, ">", $n1);
-                    }
-                    if($m1 >= 0 && ($this->_hx_index_of($text, "<img", $n1) === -1 || $this->_hx_index_of($text, "<img", $n1) > $m1)) {
-                        $m0 = $this->_hx_last_index_of($this->_hx_substr($text, $m0, $n0 - $m0), "<img", null);
-                        if($m0 >= 0) {
-                            if($this->_hx_index_of($text, $tag, $m0) > 0 && $this->_hx_index_of($text, $tag, $m0) < $n1) {
-                                $n0 = $n1;
-                                $output->add($sub);
-                                $n1 = $this->_hx_index_of($text, $tags->in_mathopen, $n0);
-                                $m0 = $m1;
-                                continue;
-                            }
-                        }
-                    }
+
+        $safeXmlCharactersEntities = [
+            tagOpener => '&laquo;',
+            tagCloser => '&raquo;',
+            doubleQuote => '&uml;',
+            realDoubleQuote => '&quot;',
+        ];
+
+        $safeXmlCharacters = [
+            tagOpener => '«',
+            tagCloser => '»',
+            doubleQuote => '¨',
+            ampersand => '§',
+            quote => '`',
+            realDoubleQuote => '¨',
+        ];
+
+        $xmlCharacters = [
+            tagOpener => '<',
+            tagCloser => '>',
+            doubleQuote => '"',
+            ampersand => '&',
+            quote => '\'',
+        ];
+
+        // Decoding entities.
+        $text = implode($safeXmlCharacters->tagOpener, explode($safeXmlCharactersEntities->tagOpener, $text));
+        $text = implode($safeXmlCharacters->tagCloser, explode($safeXmlCharactersEntities->tagCloser, $text));
+        $text = implode($safeXmlCharacters->doubleQuote, explode($safeXmlCharactersEntities->doubleQuote, $text));
+        $text = implode($safeXmlCharacters->realDoubleQuote, explode($safeXmlCharactersEntities->realDoubleQuote, $text));
+
+        // Replace safe XML characters with actual XML characters
+        $text = implode($xmlCharacters->tagOpener, explode($safeXmlCharacters->tagOpener, $text));
+        $text = implode($xmlCharacters->tagCloser, explode($safeXmlCharacters->tagCloser, $text));
+        $text = implode($xmlCharacters->doubleQuote, explode($safeXmlCharacters->doubleQuote, $text));
+        $text = implode($xmlCharacters->ampersand, explode($safeXmlCharacters->ampersand, $text));
+        $text = implode($xmlCharacters->quote, explode($safeXmlCharacters->quote, $text));
+
+        // We are replacing $ by & when its part of an entity for retrocompatibility.
+        // Now, the standard is replace § by &.
+        $returnValue = '';
+        $currentEntity = null;
+
+        $array = str_split($text);
+
+        for ($i = 0; $i < count($array); $i++) {
+            $character = $array[$i];
+            if ($currentEntity === null) {
+                if ($character === '$') {
+                    $currentEntity = '';
+                } else {
+                    $returnValue .= $character;
                 }
-                $fixUrl = new EReg("<a href=\"[^\"]*\"[^>]*>([^<]*)<\\/a>|<a href=\"[^\"]*\">", "");
-                $sub = $fixUrl->replace($sub, "\$1");
-                $sub = $this->html_entity_decode($sub);
-                $sub = str_replace($tags->in_double_quote, $tags->out_double_quote, $sub);
-                $sub = str_replace($tags->in_open, $tags->out_open, $sub);
-                $sub = str_replace($tags->in_close, $tags->out_close, $sub);
-                $sub = str_replace($tags->in_entity, $tags->out_entity, $sub);
-                $sub = str_replace($tags->in_quote, $tags->out_quote, $sub);
-                $n0 = $n1;
-                $output->add($sub);
-                $n1 = $this->_hx_index_of($text, $tags->in_mathopen, $n0);
-                unset($subtext,$e);
+            } elseif ($character === ';') {
+                $returnValue += "&$currentEntity";
+                $currentEntity = null;
+            } elseif (preg_match("([a-zA-Z0-9#._-] | '-')", $character)) { // Character is part of an entity.
+                $currentEntity .= $character;
+            } else {
+                $returnValue .= "$$currentEntity"; // Is not an entity.
+                $currentEntity = null;
+                $i -= 1; // Parse again the current character.
             }
         }
-        $output->add($this->_hx_substr($text, $n0, null));
-        return $output->b;
+
+        return $returnValue;
+
     }
 
-    private function _hx_index_of($s, $value, $startIndex = null) {
-        $x = strpos($s, $value, $startIndex);
-        if($x === false)
-            return -1;
-        else
-            return $x;
-    }
-
-    private function _hx_substr($s, $pos, $len) {
-        if($pos !== null && $pos !== 0 && $len !== null && $len < 0) return '';
-        if($len === null) $len = strlen($s);
-        if($pos < 0) {
-            $pos = strlen($s) + $pos;
-            if($pos < 0) $pos = 0;
-        } else if($len < 0 )
-            $len = strlen($s) + $len - $pos;
-        $s = substr($s, $pos, $len);
-        if($s === false)
-            return '';
-        else
-            return $s;
-    }
-
-    private function _hx_last_index_of($s, $value, $startIndex = null) {
-        $x = strrpos($s, $value, $startIndex === null ? null : strlen($s) - $startIndex);
-        if($x === false)
-            return -1;
-        else
-            return $x;
-    }
-
-    private function html_entity_decode($str) {
-        $str = str_replace("&lt;", "<", $str);
-        $str = str_replace("&gt;", ">", $str);
-        $str = str_replace("&quot;", "\"", $str);
-        $str = str_replace("&nbsp;", mb_convert_encoding('&#160;', 'UTF-8', 'HTML-ENTITIES'), $str);
-        $str = str_replace("&amp;", "&", $str);
-        return $str;
-    }
 }
