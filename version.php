6a8aeb65e47f875949735ecda21a52c443f870a4
Daniel Canet <dcanet@wiris.com>
2021-04-14 16:36:16 +0200

    Upgrade version to 7.26.0 (20210416)
    This commit bumps this plugin version to 7.26.
    We've decided to move from 7.24 to 7.26 in oder
    to synchronize version numbers with the rest of
    MathType integrations for HTML editors.
    
    Also on this commit:
    
    - Add latest Moodle versions to TravisCI pipeline.
    - Exclude PHP7_0 from TravisCI pipeline.

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020090300;
-$plugin->release = '7.24.0';
+$plugin->version = 2021041600;
+$plugin->release = '7.26.0';
 $plugin->requires = 2011120511;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'filter_wiris';
