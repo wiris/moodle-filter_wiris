eef8fbd23ead62a2e003822786258865261d94ec
Xavier Ripoll <xavier@wiris.com>
2022-11-23 11:52:23 +0100

    build(version): publish #8.0.1

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022102000;
-$plugin->release = '8.0.0';
+$plugin->version = 2022112300;
+$plugin->release = '8.0.1';
 $plugin->requires = 2011120511;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'filter_wiris';
