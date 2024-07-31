e05cacb7458b2c31787f6232db3f73f776e86677
usantos-at-wiris <ubaldo.santos@wiris.com>
2024-07-31 11:10:54 +0200

    chore: Remove new version files

--- integration/lib/com/wiris/plugin/api/RequestInformation.interface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface com_wiris_plugin_api_RequestInformation {
-	function setRequestHeaders($h);
-	function getHeaders();
-	function setHeaders($headers);
-	function setRequestAvailability($request);
-	function setRequest($request);
-}
