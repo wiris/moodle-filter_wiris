3d6dc902f6947bc438dab2440f61c7c4a09ae9af
usantos-at-wiris <ubaldo.santos@wiris.com>
2024-09-25 16:33:52 +0200

    feat: pass code linting rules

--- filter.php
+++ filter.php
@@ -1,3 +1,14 @@
 <?php
+/**
+ * This file is empty because the class filter_wiris is an alias of the class filter_wiris\text_filter.
+ * This is done to maintain compatibility with previous versions of the plugin.
+ * The new class filter_wiris\text_filter is located in the file main/filter/wiris/classes/text_filter.php.
+ *
+ * @package    filter_wiris
+ * @subpackage wiris
+ * @copyright  2023 WIRIS Europe (Maths for more S.L)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 class_alias(\filter_wiris\text_filter::class, \filter_wiris::class);
 
