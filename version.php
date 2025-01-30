014ce528bc820bf063b46c0ef27e22705cf0fa18
Ubaldo Santos <164866893+usantos-at-wiris@users.noreply.github.com>
2025-01-30 10:26:31 +0100

    build: publish #8.8.3

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025010900;
-$plugin->release = '8.8.2';
+$plugin->version = 2025013000;
+$plugin->release = '8.8.3';
 $plugin->requires = 2011120511;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'filter_wiris';
